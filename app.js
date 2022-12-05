f0c14d45af4412f9a8ad2cea77f8fe753dd542bd
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2022-12-05 20:26:22 +0530

    final

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const user = require("./models/user")
 const bcrypt = require("bcryptjs")
 const router = require("./routes/route")
 app.set("view engine", 'ejs')
-
+app.use(express.urlencoded({ extended: false}))
+app.use(require("express-session")({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+}))
 app.use(router)
 
 dbConnect().then(()=>app.listen(5000)).catch(err=>console.log(err))
